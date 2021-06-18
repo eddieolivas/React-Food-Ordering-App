75d728869bc1ea8df96fb08f2a46623fe0d4eaea
Eddie Olivas <eddie@chrysaliswebdevelopment.com>
2021-06-18 14:54:26 -0700

    Started setting up the cart provider and reducer functionality.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Header from './components/Layout/Header/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
   const [showCart, setShowCart] = useState(false);
@@ -11,13 +12,13 @@ function App() {
   };
 
   return (
-    <>
+    <CartProvider>
       {showCart && <Cart onShowCart={showCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
 
