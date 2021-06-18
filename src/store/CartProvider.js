942a31bafd7e5bba65be88602ccdd5c515f08314
Eddie Olivas <eddie@chrysaliswebdevelopment.com>
2021-06-18 15:13:15 -0700

    Set up cart reducer.

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,30 +1,45 @@
-import { useReducer } from 'react';
-import CartContext from './cart-context';
+import { useReducer } from "react";
+import CartContext from "./cart-context";
 
 const defaultCartState = {
   items: [],
-  totalAmount: 0
-}
+  totalAmount: 0,
+};
 
 const cartReducer = (state, action) => {
+  if(action.type === 'ADD') {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount = state.totalAmount + action.items.price * action.items.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    }
+  } else if (action.type === 'REMOVE') {
+
+  }
   return defaultCartState;
 };
 
-const CartProvider = props => {
-  const addItemToCartHandler = (item) => {
+const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
 
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item: item });
   };
 
   const removeItemFromCartHandler = (id) => {
-
+    dispatchCartAction({ type: "REMOVE", id: id });
   };
 
   const cartContext = {
-    items: [ ],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
-    removeItem: removeItemFromCartHandler
-  }
+    removeItem: removeItemFromCartHandler,
+  };
 
   return (
     <CartContext.Provider value={cartContext}>
