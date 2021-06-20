0283876e8db137dacf1583bff21f68981edeeac4
Eddie Olivas <eddie@chrysaliswebdevelopment.com>
2021-06-20 14:59:01 -0700

    Completed cart functionality.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,11 +11,11 @@ const Cart = (props) => {
   const hasItems = cartCtx.items.length > 0;
 
   const cartItemRemoveHandler = id => {
-
+    cartCtx.removeItem(id);
   };
 
   const cartItemAddHandler = item => {
-
+    cartCtx.addItem({...item, amount: 1});
   };
 
   return (
@@ -28,7 +28,7 @@ const Cart = (props) => {
             amount={item.amount}
             price={item.price}
             onRemove={cartItemRemoveHandler.bind(null, item.id)}
-            onAdd={cartItemAddHandler.bind(null, item)}
+            onAdd={cartItemAddHandler.bind(null, item )}
           />
         ))}
       </ul>
